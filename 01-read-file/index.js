0178e13c6e1077411f7fa3f73f07f78b69a43a32
Pavel Gordienko <guzmo6@rambler.ru>
2024-01-20 14:24:03 +0300

    feat: add solution for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "text.txt");
+const readableStream = fs.createReadStream(filePath);
+
+readableStream.on("error", (error) => {
+  console.error(`Error reading the file: ${error.message}`);
+});
+
+readableStream.on("data", (chunk) => process.stdout.write(chunk));
