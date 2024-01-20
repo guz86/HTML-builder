6198f575e08268894aa8c2b2430c0821e870da09
Pavel Gordienko <guzmo6@rambler.ru>
2024-01-20 14:29:50 +0300

    fix: brackets for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const filePath = path.join(__dirname, "text.txt");
+const filePath = path.join(__dirname, 'text.txt');
 const readableStream = fs.createReadStream(filePath);
 
-readableStream.on("error", (error) => {
+readableStream.on('error', (error) => {
   console.error(`Error reading the file: ${error.message}`);
 });
 
-readableStream.on("data", (chunk) => process.stdout.write(chunk));
+readableStream.on('data', (chunk) => process.stdout.write(chunk));
